f317a85b83739f66ef8826ee06b1c7c6dd7f9e27
Diego <diego.puyol@gmail.com>
2018-11-05 16:26:56 +0100

    no image, youtube trailer

--- src/routes/Movies/components/MoviesList.js
+++ src/routes/Movies/components/MoviesList.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Container, Row, Col, Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle } from 'reactstrap';
+import { Container, Row, Col, Badge, Media } from 'reactstrap';
+import YouTube from 'react-youtube';
+
+import NoImage from '../assets/noImage.png'
+
 
 export const MoviesList = ({ movies, results, loadingMovies }) => {
   let moviesList = null
+
+  const opts = {
+    height: '200',
+    width: '270',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: 0
+    }
+  };
 
       if(
         // results !== null &&
@@ -18,21 +30,34 @@ export const MoviesList = ({ movies, results, loadingMovies }) => {
           <Row>
               {movies.map(movie => {
                 return (
-                  <Col xs="6" sm="6" key={movie._id}>
-                    <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333', width: 'auto' }}>
-                      <CardImg src={movie.images.banner} alt={movie.title} />
-                      <CardBody>
-                        <CardTitle>{movie.title} - {movie.year}</CardTitle>
-                        <CardSubtitle>{movie.synopsis}</CardSubtitle>
-                        <CardText>{
-                          movie.genres.map((genre,i) => <div key={i}>{genre}</div>)
-                        }
-                        </CardText>
-                        {/* <CardLink href={movie.trackViewUrl}>Movie Link</CardLink>
-                        <CardLink href={movie.previewUrl}>Preview</CardLink> */}
-                      </CardBody>
-                    </Card>
-                  </Col>
+                  <Col sm='12' xs='12' key={movie._id}  style={{ textAlign: 'left' }}>
+                    <Media style={{ backgroundColor: '#333', borderColor: '#333', width: 'auto', padding:'25px' }}>
+                    <Media left>
+                      { Object.keys(movie.images).length !== 0 &&
+                        <Media object src={movie.images.banner} alt={movie.title} style={{ width: '180px' }} />
+                        ||
+                        <Media object src={NoImage} alt={movie.title} style={{ width: '180px' }} />
+                      }
+                      </Media>
+                      <Media body style={{ marginLeft:'20px', marginRight:'20px' }}>
+                        <Media left heading>
+                        {movie.title} - {movie.year}
+
+                        </Media>
+                        {movie.synopsis}
+                        <br />
+                          {
+                            movie.genres.map((genre,i) => <Badge style={{ marginTop:'20px', marginLeft:'5px' }} color="success" key={i}>{genre}</Badge>)
+                          }
+                        </Media>
+                        { movie.trailer !== null &&
+                        <Media right>
+                          <h5>Trailer</h5>
+                          <YouTube videoId={movie.trailer.split('=')[1]} opts={opts}/>
+                        </Media>
+                         }
+                    </Media>
+                 </Col>
                 )}
               )}
           </Row>
